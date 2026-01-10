74e88099a8bf9c50d25e39d9d7a68baa598c4c81
netter36 <netter36@naver.com>
2026-01-10 10:48:33 +0900

    배포 테스트 12

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite';
+// vite.config.ts
+import { defineConfig } from "vite";
 
 export default defineConfig({
   plugins: [],
-  server: { host: '0.0.0.0', port: 8000 },
+
+  // ▶︎ **여기**를 수정해 주세요.
+  base: "./", // <‑- 절대 경로 대신 상대 경로 사용
+
+  server: {
+    host: "0.0.0.0",
+    port: 8000,
+  },
   clearScreen: false,
 });
